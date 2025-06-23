9673ba6a41c2e9cd0cebe586fc15adb7260f2bad
植田佳和侑 <uedanofile@MacBook-Air.local>
2025-06-23 12:07:53 +0900

    fix:LoadingSpinnerを20px上に配置

--- src/components/general/LoadingSpinner.tsx
+++ src/components/general/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen -mt-20 md:mt-0">
       <div className="flex space-x-1">
         <div className="w-4 h-4 bg-gradient-to-br from-blue-500 to-blue-600 rounded-full animate-bounce"></div>
         <div
